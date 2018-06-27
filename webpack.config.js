0d02f81e52c0ef753112de89705c6f8e3ec37fc8
Mohamed Moughamir <moughamir@gmail.com>
2018-06-27 11:01:59 +0000

    Add Article and ArticleList components

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: ['babel-polyfill', './lib/components/index.js'],
   output: {
-      path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
   },
   module: {
